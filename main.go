88f577db46a6dd11fc158512065ef6de316abff1
Guilherme Messer <guilhermemesser@gmail.cmo>
2025-05-13 17:35:42 -0300

    Implementação da stack.go alterada para utilizar slices, implementado uma calculadora polonesa na main.go

--- main.go
+++ main.go
@@ -2,13 +2,50 @@ package main
 
 import (
 	"fmt"
-	"learning_go/stack"
+	"learning_go/stack" // ajuste para o seu módulo se for diferente
+	"strconv"
+	"strings"
 )
 
 func main() {
+	expr := "* + 2 3 4"
+	resultado := polishCalculator(expr)
+	fmt.Printf("Resultado de \"%s\" = %d\n", expr, resultado)
+}
+
+func polishCalculator(s string) int {
+	tokens := strings.Split(s, " ")
 	pilha := stack.New()
-	pilha.Push(50)
-	pilha.Push(100)
-	pilha.Pop()
-	fmt.Println(pilha)
+
+	for i := len(tokens) - 1; i >= 0; i-- {
+		token := tokens[i]
+
+		if num, err := strconv.Atoi(token); err == nil {
+			pilha.Push(num)
+		} else {
+			b := pilha.Pop()
+			a := pilha.Pop()
+			var resultado int
+
+			switch token {
+			case "+":
+				resultado = a + b
+			case "-":
+				resultado = a - b
+			case "*":
+				resultado = a * b
+			case "/":
+				if b == 0 {
+					panic("Divisão por zero")
+				}
+				resultado = a / b
+			default:
+				panic("Operador inválido: " + token)
+			}
+
+			pilha.Push(resultado)
+		}
+	}
+
+	return pilha.Pop()
 }
--- stack/stack.go
+++ stack/stack.go
@@ -1,31 +1,25 @@
 package stack
 
 type Stack struct {
-	head int
-	data [10]int
+	data []int
 }
 
 func New() Stack {
-	return Stack{head: 0}
+	return Stack{data: []int{}}
 }
 
 // Em Go é definido o receptor da função antes do nome da função
 // como por exemplo nesta função, o receptor seria (s *stack)
 // Exemplo real: <nome da stack>.push(valor inteiro)
 func (s *Stack) Push(valor int) {
-	if s.head >= len(s.data) {
-		return
-	}
-	s.data[s.head] = valor
-	s.head++
+	s.data = append(s.data, valor)
 }
 
 func (s *Stack) Pop() (valorRetirado int) {
-	if s.head == 0 {
+	if len(s.data) == 0 {
 		return 0
 	}
-	s.head-- // O head aponta para o próximo espaço vazio na fila, por isso s.head-- primeiro
-	valorRetirado = s.data[s.head]
-	s.data[s.head] = 0
-	return valorRetirado
+	valor := s.data[len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
+	return valor
 }
